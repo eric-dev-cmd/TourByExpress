041ba6a4017c25529a63bc6de3271d55b8b82f4e
zunggzing <hello.trungvinh@gmail.com>
2021-10-08 18:35:44 +0700

    Add mongoSanitize vs xss-clean

--- app.js
+++ app.js
@@ -33,6 +33,13 @@ app.use('/api', limiter);
 // Body parser, reading data from body into req.body
 app.use(express.json({ limit: '10kb' }));
 
+// Data sanitization against NoSQL query injection
+// Làm sạch dữ liệu chống lại việc đưa vào truy vấn NoSQL
+app.use(mongoSanitize());
+
+// Data sanitization against XSS - lam sach du lieu chong XSS
+app.use(xss());
+
 // Serving static files
 app.use(express.static(path.join(__dirname, 'public')));
 
