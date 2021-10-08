7d8d1d90e90d6a2223a4cc91c7ecde7bf5a7d3b7
zunggzing <hello.trungvinh@gmail.com>
2021-10-08 18:25:36 +0700

    implement rate limit

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const app = express();
 const path = require('path');
+const rateLimit = require('express-rate-limit');
 const route = require('./routes/index');
 const AppError = require('./utils/appErrors');
 const globalErrorHandler = require('./controllers/errorController');
@@ -17,6 +18,15 @@ console.log(process.env.NODE_ENV);
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
+
+// Limit requests from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many requests from this IP, please try again in an hour!',
+});
+app.use('/api', limiter);
+
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 app.use((req, res, next) => {
