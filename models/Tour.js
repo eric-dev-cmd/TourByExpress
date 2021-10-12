b0ee00e67386136a99b85a9ddefacddce81d384e
zunggzing <hello.trungvinh@gmail.com>
2021-10-12 15:47:16 +0700

    Add Modelling Tour Guides Embedding

--- models/Tour.js
+++ models/Tour.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
+const User = require('./User');
 
 const tourSchema = new mongoose.Schema(
   {
@@ -98,6 +99,12 @@ const tourSchema = new mongoose.Schema(
         day: Number,
       },
     ],
+    guides: [
+      {
+        type: mongoose.Schema.ObjectId,
+        ref: 'User',
+      },
+    ],
   },
   {
     toJSON: {
@@ -120,6 +127,11 @@ tourSchema.pre('save', function (next) {
   this.slug = slugify(this.name, {
     lower: true,
   });
+  next();
+});
+tourSchema.pre('save', async function (next) {
+  const guidesPromises = this.guides.map(async (id) => await User.findById(id));
+  this.guides = await Promise.all(guidesPromises);
   next();
 });
 // tourSchema.pre('save', function (next) {
