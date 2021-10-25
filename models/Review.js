4084ac105ffeff28102b35196fa6d2542fa23e7f
zunggzing <hello.trungvinh@gmail.com>
2021-10-25 16:59:23 +0700

    Add duplicate review

--- models/Review.js
+++ models/Review.js
@@ -63,7 +63,6 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
       },
     },
   ]);
-  console.log(stats);
 
   if (stats.length > 0) {
     await Tour.findByIdAndUpdate(tourId, {
@@ -81,6 +80,18 @@ reviewSchema.statics.calcAverageRatings = async function (tourId) {
 reviewSchema.post('save', function () {
   // this points to current review
   this.constructor.calcAverageRatings(this.tour);
+});
+// findByIdAndUpdate
+// findByIdAndDelete
+reviewSchema.pre(/^findOneAnd/, async function (next) {
+  this.r = await this.findOne();
+  // console.log(this.r);
+  next();
+});
+
+reviewSchema.post(/^findOneAnd/, async function () {
+  // await this.findOne(); does NOT work here, query has already executed
+  await this.r.constructor.calcAverageRatings(this.r.tour);
 });
 const Review = mongoose.model('Review', reviewSchema);
 
